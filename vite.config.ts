981ac0eca0cdac77af18b9afdfa93354b25b04e8
Josh <joshcotton93@gmail.com>
2025-08-29 22:24:06 -0500

    setup CORS

--- vite.config.ts
+++ vite.config.ts
@@ -22,12 +22,4 @@ export default defineConfig({
       "@": resolve(__dirname, "./src"),
     },
   },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-      },
-    },
-  },
 });
